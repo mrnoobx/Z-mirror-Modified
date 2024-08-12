bf7e3f7887ae313c1fa7baf8f3edb5eb27067232
Hrishikesh <hrishithombare15@gmail.com>
2024-08-12 14:33:46 +0530

    Removed Some cmds for mega Bots

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -444,21 +444,12 @@ async def set_commands(client):
         await client.set_bot_commands([
             BotCommand(f'{BotCommands.MirrorCommand[0]}', f'or /{BotCommands.MirrorCommand[1]} Mirror'),
             BotCommand(f'{BotCommands.LeechCommand[0]}', f'or /{BotCommands.LeechCommand[1]} Leech'),
-            BotCommand(f'{BotCommands.QbMirrorCommand[0]}', f'or /{BotCommands.QbMirrorCommand[1]} Mirror torrent using qBittorrent'),
-            BotCommand(f'{BotCommands.QbLeechCommand[0]}', f'or /{BotCommands.QbLeechCommand[1]} Leech torrent using qBittorrent'),
-            BotCommand(f'{BotCommands.YtdlCommand[0]}', f'or /{BotCommands.YtdlCommand[1]} Mirror yt-dlp supported link'),
-            BotCommand(f'{BotCommands.YtdlLeechCommand[0]}', f'or /{BotCommands.YtdlLeechCommand[1]} Leech through yt-dlp supported link'),
-            BotCommand(f'{BotCommands.CloneCommand}', 'Copy file/folder to Drive'),
-            BotCommand(f'{BotCommands.CountCommand}', '[drive_url]: Count file/folder of Google Drive.'),
             BotCommand(f'{BotCommands.StatusCommand[0]}', f'or /{BotCommands.StatusCommand[1]} Get mirror status message'),
             BotCommand(f'{BotCommands.StatsCommand[0]}', f'{BotCommands.StatsCommand[1]} Check bot stats'),
-            BotCommand(f'{BotCommands.BtSelectCommand}', 'Select files to download only torrents'),
-            BotCommand(f'{BotCommands.CategorySelect}', 'Select category to upload only mirror'),
             BotCommand(f'{BotCommands.CancelMirror}', 'Cancel a Task'),
-            BotCommand(f'{BotCommands.CancelAllCommand[0]}', f'Cancel all tasks which added by you or {BotCommands.CancelAllCommand[1]} to in bots.'),
-            BotCommand(f'{BotCommands.ListCommand}', 'Search in Drive'),
-            BotCommand(f'{BotCommands.SearchCommand}', 'Search in Torrent'),
             BotCommand(f'{BotCommands.UserSetCommand}', 'Users settings'),
+            BotCommand(f'{BotCommands.BotSetCommand}','Bot Settings[Admin Only]'),
+            BotCommand(f'{BotCommands.RestartCommand}', 'Restart Bot[Admin Only]'),
             BotCommand(f'{BotCommands.HelpCommand}', 'Get detailed help'),
         ])
 
