aaee3ef0149c97644f56e0cd06f35da66bfa54b0
Hrishikesh <hrishithombare15@gmail.com>
2025-04-04 16:12:19 +0530

    Added Metadata

--- bot/helper/mirror_utils/status_utils/metadata_status.py
+++ bot/helper/mirror_utils/status_utils/metadata_status.py
@@ -1,6 +1,8 @@
 from bot import LOGGER
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus, get_readable_time, async_to_sync
 from subprocess import run as frun
+from time import time
+from bot.helper.ext_utils.fs_utils import get_path_size
 
 def _eng_ver():
         _engine = frun(['render', '-version'], capture_output=True, text=True)
@@ -15,15 +17,28 @@ class MetadataStatus:
         self.message = listener.message
         self.engine = f'FFmpeg v{_eng_ver()}'
         self.extra_details = self.__listener.extra_details
+        self.__start_time = time()
 
     def gid(self):
         return self.__gid
 
     def progress(self):
-        return '0'
+        return f'{round(self.progress_raw(), 2)}%'
+    
+    def processed_raw(self):
+        return async_to_sync(get_path_size, self.__listener.dir) - self.__size
 
     def speed(self):
-        return '0'
+        return f'{get_readable_file_size(self.speed_raw())}/s'
+    
+    def speed_raw(self):
+        return self.processed_raw() / (time() - self.__start_time)
+
+    def progress_raw(self):
+        try:
+            return self.processed_raw() / self.__size * 100
+        except:
+            return 0
 
     def name(self):
         return self.__name
@@ -32,13 +47,17 @@ class MetadataStatus:
         return get_readable_file_size(self.__size)
 
     def eta(self):
-        return '0s'
+        try:
+            seconds = (self.__size - self.processed_raw()) / self.speed_raw()
+            return f'{get_readable_time(seconds)}'
+        except:
+            return '-'
 
     def status(self):
         return MirrorStatus.STATUS_METADATA
 
     def processed_bytes(self):
-        return 0
+        return get_readable_file_size(self.processed_raw())
 
     def download(self):
         return self
@@ -51,4 +70,4 @@ class MetadataStatus:
             except:
                 pass
         self.__listener.suproc = 'cancelled'
-        await self.__listener.onUploadError('Metada edit stopped by user!')
+        await self.__listener.onUploadError('Metadata edit stopped by user!')
